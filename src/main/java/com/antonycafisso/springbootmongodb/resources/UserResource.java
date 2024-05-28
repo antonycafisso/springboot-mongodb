fcd0f2a6dcc7cf1a4864b95b542e950f72e94844
Antony Cafisso <tonycafisso@gmail.com>
2024-05-27 23:07:08 -0300

    find User by id

--- src/main/java/com/antonycafisso/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/antonycafisso/springbootmongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.antonycafisso.springbootmongodb.services.UserService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,9 @@ public class UserResource {
         return ResponseEntity.ok().body(listDto);
     }
 
-
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
+    }
 }
--- src/main/java/com/antonycafisso/springbootmongodb/services/UserService.java
+++ src/main/java/com/antonycafisso/springbootmongodb/services/UserService.java
@@ -2,10 +2,13 @@ package com.antonycafisso.springbootmongodb.services;
 
 import com.antonycafisso.springbootmongodb.domain.User;
 import com.antonycafisso.springbootmongodb.repositories.UserRepository;
+import com.antonycafisso.springbootmongodb.services.exception.ObjectNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +17,10 @@ public class UserService {
 
     public List<User> findAll (){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.orElseThrow(() -> new ObjectNotFoundException("User not found"));
     }
 }
